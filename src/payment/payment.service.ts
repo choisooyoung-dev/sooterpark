57b78a42022128d6c527470dd07aabf614185270
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2024-01-01 05:01:20 +0900

    ✨ update: 포인트

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -8,6 +8,7 @@ import { Performance } from 'src/performance/entities/performance.entity';
 import { CreateSeatDto } from './../seat/dto/create-seat.dto';
 import { Payment } from './entities/payment.entity';
 import { Schedule } from 'src/schedule/entities/schedule.entity';
+import { Point } from 'src/point/entities/point.entity';
 
 @Injectable()
 export class PaymentService {
@@ -20,12 +21,14 @@ export class PaymentService {
     private performanceRepository: Repository<Performance>,
     @InjectRepository(Schedule)
     private scheduleRepository: Repository<Schedule>,
+    @InjectRepository(Point)
+    private pointRepository: Repository<Point>,
     private dataSource: DataSource,
   ) {}
   async create(
     user: any,
-    schedule_id: number,
-    performance_id: number,
+    schedule_id: any,
+    performance_id: any,
     createPaymentDto: CreatePaymentDto,
     createSeatDto: CreateSeatDto,
   ) {
@@ -40,31 +43,65 @@ export class PaymentService {
       console.log('performance_id ==> ', performance_id);
 
       const targetPerformance = await this.performanceRepository.findOne({
-        where: { id: performance_id },
+        where: { id: +performance_id },
       });
 
       const targetSchedule = await this.scheduleRepository.find({
-        where: { performance: { id: performance_id } },
+        where: { performance: { id: +performance_id } },
+      });
+
+      const getSheduleWithId = await this.scheduleRepository.find({
+        where: { id: schedule_id },
+      });
+
+      // 스케쥴 아이디에 따른 좌석들
+      const getSeatCountWithScheduleId = await this.seatRepository.find({
+        where: { schedule: { id: +schedule_id } },
       });
 
       console.log('targetPerformance: ', targetPerformance);
-      console.log('targetSchedule: ', targetSchedule);
+
+      console.log('targetPerformance[0].price: ', targetPerformance.price);
 
       let total_price = 0;
 
+      if (!targetPerformance) {
+        // targetPerformance가 null인 경우 예외 처리
+        throw new Error('해당하는 공연이 없습니다.');
+      }
+
       const newPayment = await queryRunner.manager.save(Payment, {
-        performance: targetPerformance.id,
+        performance: { id: +performance_id },
         total_price,
         user_id: user.id,
       });
 
+      console.log('targetPerformance id: ', targetPerformance.price);
+      let totalSeatPrice = 0;
       for (let i = 0; i < seats.length; i++) {
         const newGrade = seats[i].grade;
-        console.log('newGrade: ', newGrade);
         const newSeatNum = seats[i].seat_num;
-        console.log('newSeatNum: ', newSeatNum);
-        console.log('스케쥴 아이디 ===> ', schedule_id);
-        console.log('유저 아이디 ===> ', user.id);
+
+        let seatPriceWithGrade: number = 0;
+        if (
+          newGrade === 'V' &&
+          getSeatCountWithScheduleId.length < getSheduleWithId[0].vip_seat_limit
+        ) {
+          seatPriceWithGrade = targetPerformance.price * 1.75;
+        } else if (
+          newGrade === 'R' &&
+          getSeatCountWithScheduleId.length <
+            getSheduleWithId[0].royal_seat_limit
+        ) {
+          seatPriceWithGrade = targetPerformance.price * 1.25;
+        } else if (
+          newGrade === 'S' &&
+          getSeatCountWithScheduleId.length <
+            getSheduleWithId[0].standard_seat_limit
+        ) {
+          seatPriceWithGrade = targetPerformance.price;
+        }
+
         // 좌석이 예매됐는지 확인
         // 됐으면 payment도 x
         const reservedSeat = await queryRunner.manager.findOne(Seat, {
@@ -83,17 +120,32 @@ export class PaymentService {
           grade: newGrade,
           seat_num: newSeatNum,
           performance: targetPerformance.id,
-          seat_price: targetPerformance.price, // seat_price 값을 targetPerformance.price로 설정
+          seat_price: seatPriceWithGrade, // seat_price 값을 targetPerformance.price로 설정
           user: { id: user.id },
         });
-        total_price += targetPerformance.price;
+        totalSeatPrice += seatPriceWithGrade;
         console.log(newSeat);
       }
+      console.log(totalSeatPrice);
+      const lastPoint = await queryRunner.manager.find(Point, {
+        where: { user: { id: user.id } },
+        order: { id: 'DESC' },
+        take: 1,
+      });
+      console.log(lastPoint);
+      await queryRunner.manager.save(Point, {
+        user: { id: user.id },
+        payment: { id: newPayment.id },
+        deposit: 0,
+        withdraw: totalSeatPrice,
+        balance: lastPoint[0].balance - totalSeatPrice,
+      });
 
       await queryRunner.commitTransaction();
       return { success: true, message: 'Reservation successful', total_price };
     } catch (error) {
       // 롤백 시에 실행할 코드 (예: 로깅)
+      console.error('Error during reservation:', error);
       await queryRunner.rollbackTransaction();
     } finally {
       // 사용이 끝난 후에는 항상 queryRunner를 해제합니다.
